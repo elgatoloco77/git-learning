73db214ae71a95a0cbe2d2e674ac5e4a8184b4be
Héctor Valles <hector.valles@outlook.com>
2025-09-19 08:35:03 -0600

    feat:for

--- app.py
+++ app.py
@@ -3,4 +3,12 @@ import socket
 hostname = socket.gethostname()
 print(f"Hostname: {hostname}")
 
+IPAddress = socket.gethostbyname(hostname)
+print(f"IP Address: {IPAddress}")
+
+for i in range(10):
+    print(i
+
+
+
 print("Hola")
