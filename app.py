35bb6ac3a322b4bdde99b17834d8490f9bd95b93
Héctor Valles <hector.valles@outlook.com>
2025-09-22 08:28:49 -0600

    faet:suma

--- app.py
+++ app.py
@@ -9,3 +9,7 @@ print(f"IP Address: {IPAddress}")
 for i in range(10):
     print(i)
 print("Hola")
+
+numero_a = input("Dame el primer numero: ")
+numero_b = input("Dame el segundo numero: ")
+print(f"El resultado es: ")
